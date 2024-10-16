eebd0ff19b1c0ad55969cefa76089ac6d2305e6a
kiwonbyun <bkw9603@gmail.com>
2024-10-16 18:18:06 +0900

    map, vector

--- Vector3.cpp
+++ Vector3.cpp
@@ -1,43 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <algorithm>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// #include <cstdlib>
+// #include <ctime>
+// #include <algorithm>
+// using namespace std;
 
-int main(){
-    srand((unsigned)time(NULL));
+// int main(){
+//     srand((unsigned)time(NULL));
 
-    vector<int> iv1(5);
-    cout << "vector 1 : ";
-    for(auto& i : iv1){
-        i = rand()%100;
-        cout << i << " "; 
-    }
-    sort(iv1.begin(), iv1.end());
+//     vector<int> iv1(5);
+//     cout << "vector 1 : ";
+//     for(auto& i : iv1){
+//         i = rand()%100;
+//         cout << i << " "; 
+//     }
+//     sort(iv1.begin(), iv1.end());
 
-    cout << endl << "sorted vector 1 : ";
-    for(auto i : iv1){
-        cout << i << " "; 
-    }
+//     cout << endl << "sorted vector 1 : ";
+//     for(auto i : iv1){
+//         cout << i << " "; 
+//     }
 
-    vector<int> iv2(5);
-    cout << endl << "vector 2 : ";
-    for(auto& i : iv2){
-        i = rand()%100;
-        cout << i << " "; 
-    }
-    sort(iv2.begin(), iv2.end());
+//     vector<int> iv2(5);
+//     cout << endl << "vector 2 : ";
+//     for(auto& i : iv2){
+//         i = rand()%100;
+//         cout << i << " "; 
+//     }
+//     sort(iv2.begin(), iv2.end());
 
-    cout << endl << "sorted vector 2 : ";
-    for(auto i : iv2){
-        cout << i << " "; 
-    }
+//     cout << endl << "sorted vector 2 : ";
+//     for(auto i : iv2){
+//         cout << i << " "; 
+//     }
 
-    vector<int> iv3(iv1.size() + iv2.size());
-    merge(iv1.begin(), iv1.end(), iv2.begin(), iv2.end(), iv3.begin());
-    cout << "벡터1과 벡터2를 합병한 결과 : ";
-    for(auto i : iv3){
-        cout << i << " ";
-    }
-}
+//     vector<int> iv3(iv1.size() + iv2.size());
+//     merge(iv1.begin(), iv1.end(), iv2.begin(), iv2.end(), iv3.begin());
+//     cout << "벡터1과 벡터2를 합병한 결과 : ";
+//     for(auto i : iv3){
+//         cout << i << " ";
+//     }
+// }
